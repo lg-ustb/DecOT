b34f30858a0021e455e2d99bb2709e1c49481ed2
lg-ustb <lg_ustb@163.com>
2021-12-30 17:40:31 +0800

    README.md

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup
 
 NAME = 'DecOT'
-VERSION = '1.0'
+VERSION = '1.0.1'
 PACKAGES = ['DecOT']
 
-setup(name=NAME
-      , version=VERSION
-      , packages=PACKAGES
+setup(name=NAME,
+      version=VERSION,
+      packages=PACKAGES,
+      python_requires=">=3.6.0",
+
+      url="https://github.com/lg-ustb/DecOT",
+      author="lg_ustb",
+      author_email="lg_ustb@163.com",
+
+      install_requires=["pandas", "numpy", 'scipy', 'POT', 'rpy2', 'multiprocess', 'matlab'],
       )
